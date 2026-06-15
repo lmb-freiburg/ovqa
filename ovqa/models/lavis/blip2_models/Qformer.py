09bf20a5efaa5452881d64596f5a908fbf869122
gings <s@ging.ai>
2026-06-15 10:11:52 +0200

    fix transformers import

--- ovqa/models/lavis/blip2_models/Qformer.py
+++ ovqa/models/lavis/blip2_models/Qformer.py
@@ -20,11 +20,10 @@ from transformers.modeling_outputs import (
     CausalLMOutputWithCrossAttentions,
     MaskedLMOutput,
 )
+from transformers import apply_chunking_to_forward
+from transformers.pytorch_utils import find_pruneable_heads_and_indices, prune_linear_layer
 from transformers.modeling_utils import (
     PreTrainedModel,
-    apply_chunking_to_forward,
-    find_pruneable_heads_and_indices,
-    prune_linear_layer,
 )
 from transformers.models.bert.configuration_bert import BertConfig
 from transformers.utils import logging
